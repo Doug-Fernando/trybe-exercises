37e891c6a0b8567d281274917bbc921cb20b0b34
Douglas Fernando <doug.fernando95@gmail.com>
2020-10-26 02:07:27 -0300

    Exercise 5: Complete.

---
+++ FRONT-END/block_4/dia_1/exercise5.js
@@ -0,0 +1,15 @@
+let a = 80;
+let b = 60;
+let c = 40;
+
+const sum = 180;
+
+if(sum > 180 || sum < 180){    
+    console.log('False');
+    return false;
+}else if (sum == 180) {   
+    console.log('True');
+    return true;
+} else {
+    console.log('error, wrong values');
+}
