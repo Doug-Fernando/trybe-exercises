f7c03cc724b14c72a4cf293310b209ec216c1642
Douglas Fernando <doug.fernando95@gmail.com>
2020-10-27 18:26:53 -0300

    Exercise 4 : Complete.

--- FRONT-END/block_4/dia_2/exercise3-and-4.js
+++ FRONT-END/block_4/dia_2/exercise3-and-4.js
@@ -1,3 +1,4 @@
+// Exercise 3:
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 
@@ -8,3 +9,11 @@ for (let i = 0; i < numbers.length; i += 1) {
 sum = sum / numbers.length;
 
 console.log(sum);
+
+// Exercise 4:
+
+if(sum > 20){
+  console.log("Valor final maior que 20");
+} else {
+  console.log("Valor final menor que 20");
+}
