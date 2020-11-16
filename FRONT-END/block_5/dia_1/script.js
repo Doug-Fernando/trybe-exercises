4e5dc785a68bd9deec0c1c9f2b0458f480eba16f
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-15 22:21:13 -0300

    gitignore e atualizando bloco 4 e 5

---
+++ FRONT-END/block_5/dia_1/script.js
@@ -0,0 +1,19 @@
+let paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerText = 'Horizon Zero Dawn';
+// Pega um elemento pelo ID
+document.getElementById('subtitle').innerHTML = 'Jogo muito dahora';
+
+
+document.getElementById('second-paragraph').innerText = 'PS4 Pro';
+
+
+// document.getElementsByClassName('className');
+    // Pega elementos pelo Class. Tem como Retorno um array e deve ser tratado como.
+
+// document.getElementsByTagName('div');
+    // Pega elementos pela TAG. Tem como retorno um array e deve ser tratado como.
+
+document.getElementsByClassName('className')[0].style.color = 'Brown';
+document.getElementsByTagName('h4')[0].style.color = 'gray';
