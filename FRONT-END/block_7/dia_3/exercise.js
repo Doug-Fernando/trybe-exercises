fb86fbea35c4e3c5b23095f3ed41e65ea4e9decc
Douglas Fernando <doug.fernando95@gmail.com>
2020-11-25 18:44:18 -0300

    Primeira parte do exercicio

---
+++ FRONT-END/block_7/dia_3/exercise.js
@@ -0,0 +1,31 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => {
+  sum(4, '5');
+});
+// console.log(sum(4, '5'));
+
+// ---------------------
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
