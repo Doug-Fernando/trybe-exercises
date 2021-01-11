7e1592f8f1bb28a81ec7d7cb4941016e819d133d
Doug-Fernando <doug.fernando95@gmail.com>
2021-01-11 17:15:17 -0300

    Exercise block 11 day 1

--- Front-end Development/block_11/day_1/src/App.js
+++ Front-end Development/block_11/day_1/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HellowWorld from './HelloWorld';
+import Component from './Component';
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+const myArray = ['React', 'HTMl', 'CSS', 'JavaScript', 'PHP'];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div>
+    <HellowWorld />
+    <Component />
+
+    <div>Exercise:</div>
+    {myArray.map(item => task(item))};
+  </div>);
 }
 
 export default App;
---
+++ Front-end Development/block_11/day_1/src/HelloWorld.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class HellowWorld extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+export default HellowWorld;
